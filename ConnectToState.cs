a71a6d81ced81e7dbd7b8cc6acc6296373f86028
mcgear <michael.gearhardt@gmail.com>
2020-03-19 11:35:35 -0600

    Latest

--- ConnectToState.cs
+++ ConnectToState.cs
@@ -1,33 +1,29 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System.Security.Claims;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using LCU.StateAPI;
-using Fathym;
-using LCU.StateAPI.Utilities;
 using Microsoft.WindowsAzure.Storage.Blob;
-using Fathym.Design.Factory;
+using LCU.StateAPI.Utilities;
 
 namespace LCU.State.API.NapkinIDE.UserManagement
 {
     public static class ConnectToState
     {
         [FunctionName("ConnectToState")]
-        public static async Task<ConnectToStateResponse> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequest req, ILogger logger,
+        public static async Task<ConnectToStateResponse> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequest req, ILogger log,
             ClaimsPrincipal claimsPrincipal, //[LCUStateDetails]StateDetails stateDetails,
             [SignalR(HubName = UserManagementState.HUB_NAME)]IAsyncCollector<SignalRMessage> signalRMessages,
             [SignalR(HubName = UserManagementState.HUB_NAME)]IAsyncCollector<SignalRGroupAction> signalRGroupActions,
             [Blob("state-api/{headers.lcu-ent-api-key}/{headers.lcu-hub-name}/{headers.x-ms-client-principal-id}/{headers.lcu-state-key}", FileAccess.ReadWrite)] CloudBlockBlob stateBlob)
         {
-            return await signalRMessages.ConnectToState<UserManagementState>(req, logger, claimsPrincipal, stateBlob, signalRGroupActions);
+            return await signalRMessages.ConnectToState<UserManagementState>(req, log, claimsPrincipal, stateBlob, signalRGroupActions);
         }
     }
 }
